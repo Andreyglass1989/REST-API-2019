b13b2c8448407ee3488c91957adb670009f5ff69
Andreyglass1989 <1989andreyglass@gmail.com>
2019-02-11 22:35:51 +0200

    4.A JSON Response

--- updates/views.py
+++ updates/views.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
+from .models import Update
 # Create your views here.
+
+# def detail_view(request):
+	# return render(request, template, {}) #return JSON data
+	# return HttpResponse(get_template().render({}))
+
+def update_model_detail_view(request):
+	data= {
+		"count": 1000,
+		"content": "Some new content"
+	}
+	return JsonResponse(data)
