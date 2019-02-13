e74c703f33d4bba8a76ff648620067901dfacefc
Andreyglass1989 <1989andreyglass@gmail.com>
2019-02-13 22:00:08 +0200

    6. JSON Class Based View and JSON Response Mixin

--- updates/views.py
+++ updates/views.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.views.generic import View
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
 import json
 from .models import Update
+from restapi.mixins import JsonResponseMixin
 # Create your views here.
 
 # def detail_view(request):
@@ -20,3 +22,22 @@ def json_example_view(request):
 	json_data = json.dumps(data)
 	# return JsonResponse(data)
 	return HttpResponse(json_data, content_type='application/json')
+
+
+class JsonCBV(View):
+	def get(self, request, *args, **kwargs):
+		data= {
+			"count": 1000,
+			"content": "Some new content"
+		}
+		# json_data = json.dumps(data)
+		return JsonResponse(data)
+		# return HttpResponse(json_data, content_type='application/json')
+
+class JsonCBV2(JsonResponseMixin, View):
+	def get(self, request, *args, **kwargs):
+		data= {
+			"count": 1000,
+			"content": "Some new content"
+		}
+		return self.render_to_json_response(data)
